ba45948595e87aee4ec4e82ff4135c00d201a451
Artem Ponomarev <garorobe@gmail.com>
2018-09-15 00:38:51 +0300

    [iocage-plugin-jackett]: Initial commit; An example of Freenas v2 plugin from existing jail script

---
+++ post_install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ln -s /usr/local/bin/mono /usr/bin/mono
+fetch https://github.com/Jackett/Jackett/releases/download/v0.9.23/Jackett.Binaries.Mono.tar.gz -o /usr/local/share
+tar -xzvf /usr/local/share/Jackett.Binaries.Mono.tar.gz -C /usr/local/share
+rm /usr/local/share/Jackett.Binaries.Mono.tar.gz
+pw user add jackett -c jackett -u 818 -d /nonexistent -s /usr/bin/nologin
+mkdir /var/db/jackett
+chown -R jackett:jackett /usr/local/share/Jackett /var/db/jackett
+mkdir /usr/local/etc/rc.d
+
+chmod u+x /usr/local/etc/rc.d/jackett
+sysrc "jackett_enable=YES"
+service jackett restart
